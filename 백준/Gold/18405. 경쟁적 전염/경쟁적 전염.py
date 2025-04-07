05e6eca9a73d6d57469733d5c4167949f7b2279d
SoominChung <ssoomin200@gmail.com>
2025-04-07 10:08:47 -0400

    [Gold V] Title: 경쟁적 전염, Time: 148 ms, Memory: 35580 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,38 +1,41 @@
 from collections import deque
 
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+
 n, k = map(int, input().split())
 
 maps = []
-#virus_dict = {i: deque() for i in range(0,k+1)}
-virus_dict = {i: [] for i in range(0,k+1)}
+queue = []
 for i in range(n):
     maps.append(list(map(int, input().split())))
     for j in range(n):
         val = maps[i][j]
-        virus_dict[val].append((i,j)) # virus 종류별로 어디 index에 있는지 정보 모아두기
-s, final_x, final_y = map(int, input().split())
+        if val != 0: # 바이러스라면 정보 저장해둠!
+            queue.append((val, i, j))
+S, final_x, final_y = map(int, input().split())
 final_x -=1
 final_y -=1
 
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
-for second in range(s):
-    new_virus_dict = {i: [] for i in range(0, k+1)} 
-    for k_ in range(1,k+1):
-        for x,y in virus_dict[k_]: # k_ 바이러스가 존재하는 인덱스들을 모두 순회
-            for i in range(4):
-                nx = x+dx[i]
-                ny = y+dy[i]
-                if 0<=nx<n and 0<=ny<n and maps[nx][ny] == 0:
-                    maps[nx][ny] = k_
-                    '''
-                    # 아래처럼 순회 도중에 deque 수정하면 RuntimeError: deque mutated during iteration
-                    virus_dict[k_].popleft() # 이미 증식한건 굳이 또 안돌아도되니까
-                    virus_dict[k_].append((nx,ny))
-                    '''
-                    new_virus_dict[k_].append((nx,ny))
-        #virus_dict[k_] = virus_dict[k_][len(virus_dict[k_]):] # 원래 있던건 없애고 새로운 것만 남게 -> 그냥 새로운 걸 만들어서 갱신하기
-        virus_dict[k_] = new_virus_dict[k_]
+queue.sort() #sort 사용하기 위해서 우선 list로 정의하고 이후에 deque로 바꿔줌
+queue = deque(queue)
+
+time = 0 
+while queue:
+    if time == S:
+        break
+
+    for _ in range(len(queue)):
+            
+        k_, x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0<=nx<n and 0<=ny<n and maps[nx][ny] == 0:
+                maps[nx][ny] = k_
+                queue.append((k_, nx, ny))
+    time += 1    
 if maps[final_x][final_y]:
     print(maps[final_x][final_y])
 else:
