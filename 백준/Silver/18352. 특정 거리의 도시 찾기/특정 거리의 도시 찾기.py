cde1155c958149470c0c6e9ccdbeb45aaec2dbc3
SoominChung <ssoomin200@gmail.com>
2025-04-07 04:13:35 -0400

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1952 ms, Memory: 100836 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,38 @@
+# 최단거리를 묻고, 간선의 가중이 모두 동일하니 BFS로 해결   
+
+from collections import deque
+import sys
+
+#n, m, k, x = map(int,input().split())
+n,m,k,x = list(map(int, sys.stdin.readline().split()))
+
+# 전체 도로 정보 (단방향 도로를 인접 리스트로 표현)
+graph = [[] for _ in range(n+1)]
+for i in range(m):
+    #a,b = map(int, input().split())
+    a,b = list(map(int, sys.stdin.readline().split()))
+    graph[a].append(b)
+
+# 도시별 x부터의 거리 저장
+distance = [-1]*(n+1)
+distance[x] = 0
+
+queue = deque([x])
+while queue:
+    # 현재 도시
+    now = queue.popleft()
+    
+    # 인접 도시들에 대해 거리 저장
+    for i in graph[now]:
+        # 처음 방문한 도시라면 거리 저장해주고 queue에 넣음
+        if distance[i]==-1:
+            distance[i] = distance[now]+1
+            queue.append(i)
+
+check = False            
+for i in range(1,n+1):
+    if distance[i] == k:
+        print(i)
+        check =True
+if check ==False:
+    print(-1)
