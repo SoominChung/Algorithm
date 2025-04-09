9a63f18c7865b554ff4feb588fe2c7b1a704d19f
SoominChung <ssoomin200@gmail.com>
2025-04-09 09:04:41 -0400

    [Silver I] Title: 연산자 끼워넣기, Time: 1048 ms, Memory: 117596 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,28 +1,44 @@
+
+from itertools import permutations
+
 N = int(input())
 number = list(map(int, input().split()))
-p,s,m,d = map(int,input().split())
+p,m,s,d = map(int,input().split())
+total_calc = []
+for _ in range(p):
+    total_calc.append(0)
+for _ in range(m):
+    total_calc.append(1)
+for _ in range(s):
+    total_calc.append(2)
+for _ in range(d):
+    total_calc.append(3)
 
-min_val,max_val = 2e9,-2e9
-
-def dfs(depth,num,plus,substract,multiply,divide):
-    global min_val, max_val
-    if depth == N: # 끝까지 탐색해야지
-        min_val = min(min_val,num)
-        max_val = max(max_val,num)
-        return
-
-    if plus:
-        dfs(depth+1,num+number[depth],plus-1,substract,multiply,divide)
-    if substract:
-        dfs(depth+1,num-number[depth],plus,substract-1,multiply,divide)
-    if multiply:
-        dfs(depth+1,num*number[depth],plus,substract,multiply-1,divide)
-    if divide:
-        if num <0:
-            dfs(depth+1,-(-num//number[depth]),plus,substract,multiply,divide-1)
+def calculataion(cc, x,y):
+    if cc == 0:
+        return x+y
+    elif cc == 1:
+        return x-y
+    elif cc == 2:
+        return x*y
+    else:
+        if x>=0:
+            return x // y
         else:
-            dfs(depth+1,num//number[depth],plus,substract,multiply,divide-1)
+            #return -1*x // y * (-1)
+            return int(x/y)
 
-dfs(1, number[0], p,s,m,d)
+cases = list(set(permutations(total_calc,N-1)))
+min_val, max_val = 1e9+2,-1e9-4
+for case_ in cases:
+    # 하나의 케이스에 대해
+
+    # 함수 활용해서 생으로 계산할 때
+    tmp = number[0]
+    for i in range(1,N):
+        tmp = calculataion(list(case_)[i-1], tmp, number[i])
+
+
+    min_val, max_val = min(min_val, tmp), max(max_val, tmp)
 print(max_val)
 print(min_val)
